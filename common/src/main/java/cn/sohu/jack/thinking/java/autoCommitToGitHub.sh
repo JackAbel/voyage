f03da5146b52a69f09a230c74e972a6aaf24ce05
Jin <xiangbaojin215867@sohu-inc.com>
2019-03-01 21:31:02 +0800

    boy, good work!

---
+++ common/src/main/java/cn/sohu/jack/thinking/java/autoCommitToGitHub.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# commit voyage to github on 22:00 weekday
+
+cd /Users/JackKing/work/Idea_Projects/voyage/
+commit_date=`date -d %Y%m%d%H`
+
+git commit -am "boy, good work!"
+echo "push is success!"
+git push
+
+exit $?
