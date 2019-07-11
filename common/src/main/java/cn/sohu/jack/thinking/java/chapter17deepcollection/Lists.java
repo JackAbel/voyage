0852a69ffc036a241ceaccce94a63f536da5c6ee
xiangbaojin <xiangbaojin215867@sohu-inc.com>
2019-07-11 20:55:18 +0800

    new file

---
+++ common/src/main/java/cn/sohu/jack/thinking/java/chapter17deepcollection/Lists.java
@@ -0,0 +1,9 @@
+package cn.sohu.jack.thinking.java.chapter17deepcollection;
+
+/**
+ * @description:
+ * @author: Xiangbao Jin
+ * @since 2019/7/8 4:11 PM
+ */
+public class Lists {
+}
---
+++ common/src/main/java/cn/sohu/jack/thinking/java/chapter17deepcollection/Practice7.java
@@ -0,0 +1,9 @@
+package cn.sohu.jack.thinking.java.chapter17deepcollection;
+
+/**
+ * @description:
+ * @author: Xiangbao Jin
+ * @since 2019/7/8 5:12 PM
+ */
+public class Practice7 {
+}
